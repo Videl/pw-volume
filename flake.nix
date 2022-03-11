811edfe8c98db76f33f9a0387198138552d16e54
Thibaut SMITH <t.smith@planet-work.com>
2022-03-11 20:54:42 +0100

    adds: rec

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
   let
     pkgs = import nixpkgs { inherit system; };
     naersk-lib = pkgs.callPackage naersk {};
-  in {
+  in rec {
 
     # `nix build`
     packages.pw-volume = naersk-lib.buildPackage {
